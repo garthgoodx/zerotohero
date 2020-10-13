b1dbd36a86c7a80128aa60776a9181f5eb4e367c
garthgoodx <garth@goodx.co.za>
2020-10-13 08:11:28 +0200

    test init ewrwerwerew

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     logger.info('sssssssssssssssssss')
-    print_hi('PyCharm')
+    print_hi('PyChafddddddddddddrm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
